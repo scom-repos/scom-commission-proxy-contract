33421d2ac7f69fc36cef08b44b3ccc6c495e6cdd
benho-ijs <benho@instajs.com>
2022-11-30 18:42:00 +0800

    simple version

--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -25,6 +25,8 @@ contract Proxy {
         distributor = _distributor;
     }
 
+    receive() external payable {}
+
     function _transferAssetFrom(IERC20 token, uint256 amount) internal returns (uint256 balance) {
         balance = token.balanceOf(address(this));
         token.safeTransferFrom(msg.sender, address(this), amount);
@@ -33,6 +35,68 @@ contract Proxy {
     function safeTransferETH(address to, uint value) internal {
         (bool success,) = to.call{value:value}(new bytes(0));
         require(success, 'TransferHelper: ETH_TRANSFER_FAILED');
+    }
+
+    function tokenIn(address target, TokensIn calldata tokensIn, bytes memory data) public payable {
+        // transfer token to target 
+        TokensIn calldata transfer = tokensIn;
+        uint256 totalCommissions;
+        uint256 length2 = transfer.commissions.length;
+        for (uint256 j = 0 ; j < length2 ; j++ ){
+            totalCommissions += transfer.commissions[j].amount;
+        }
+        uint amount = transfer.amount - totalCommissions;
+
+        // transfer commissions to distributor
+        transfer.token.safeTransferFrom(msg.sender, address(distributor), totalCommissions);
+        distributor.addCommissions(transfer.token, transfer.commissions);
+
+        // transfer / approve tokens to target
+        if (transfer.directTransfer) {
+            transfer.token.safeTransferFrom(msg.sender, target, amount);
+        } else {
+            amount = _transferAssetFrom(transfer.token, amount);
+            // optional, may make a list of tokens need to reset first
+            transfer.token.safeApprove(target, 0);
+            transfer.token.safeApprove(target, amount);
+        }
+        emit TransferForward(target, transfer.token, msg.sender, amount, totalCommissions);
+
+        assembly {
+            let ret := call(gas(), target, 0, add(0x20,data), mload(data), 0, 0)
+            // returndatacopy(0, 0, returndatasize())
+            if eq(0, ret) {
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+            returndatacopy(0, 0, returndatasize())
+            return(0, returndatasize())
+        }
+    }
+    function ethIn(address target, IDistributor.Commission[] calldata commissions, bytes memory data) public payable {
+        // transfer ETH to target 
+        uint256 totalCommissions;
+        uint256 length2 = commissions.length;
+        for (uint256 j = 0 ; j < length2 ; j++ ){
+            totalCommissions += commissions[j].amount;
+        }
+        uint amount = msg.value - totalCommissions;
+
+        // transfer commissions to distributor
+        distributor.addCommissions{value:totalCommissions}(IERC20(address(0)), commissions);
+
+        emit TransferForward(target, IERC20(address(0)), msg.sender, amount, totalCommissions);
+
+        assembly {
+            let ret := call(gas(), target, amount, add(0x20,data), mload(data), 0, 0)
+            // returndatacopy(0, 0, returndatasize())
+            if eq(0, ret) {
+                returndatacopy(0, 0, returndatasize())
+                revert(0, returndatasize())
+            }
+            returndatacopy(0, 0, returndatasize())
+            return(0, returndatasize())
+        }
     }
 
     function proxyCall(address target, TokensIn[] calldata tokensIn, address to, IERC20[] calldata tokensOut, bytes memory data) public payable {
@@ -79,18 +143,6 @@ contract Proxy {
             emit TransferForward(target, transfer.token, msg.sender, amount, totalCommissions);
         }
 
-        // call target
-        //  uint256 inOffset = // selector q + referrer + target
-        //                     4 + 0x20 + 0x20 + 
-        //                    // tokenIns + amounts + directTransfer
-        //                    (3 * 0x20 * (length + 2)) + 
-        //                    // to
-        //                    0x20 +
-        //                    // tokensOut  
-        //                    (0x20 * (tokensOut.length +2)) +
-        //                    // data (pointer / length)
-        //                    0x20 + 0x20;
-        //  uint256 insize = data.length;
         assembly {
             let ret := call(gas(), target, ethAmount, add(0x20,data), mload(data), 0, 0)
             // returndatacopy(0, 0, returndatasize())
@@ -114,8 +166,8 @@ contract Proxy {
             emit TransferBack(target, tokensOut[i], to, amount);
         }
         assembly {
-             returndatacopy(0, 0, returndatasize())
-             return(0, returndatasize())
+            returndatacopy(0, 0, returndatasize())
+            return(0, returndatasize())
         }
     }
 }
